9654d30b3be65c1db65d5ff915e77da34ddee6c4
sashashtmv <sasha-shtmv@rambler.ru>
2018-08-18 16:21:05 +0300

    new branch1

--- Workout/app/src/main/java/com/sashashtmv/workout/DetailActivity.java
+++ Workout/app/src/main/java/com/sashashtmv/workout/DetailActivity.java
@@ -9,5 +9,8 @@ public class DetailActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
+
+        WorkoutDetailFragment detailFragment = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_fragment);
+        detailFragment.setWorkoutID(1);
     }
 }
--- Workout/app/src/main/java/com/sashashtmv/workout/WorkoutDetailFragment.java
+++ Workout/app/src/main/java/com/sashashtmv/workout/WorkoutDetailFragment.java
@@ -6,6 +6,7 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 /**
@@ -13,7 +14,7 @@ import android.view.ViewGroup;
  */
 public class WorkoutDetailFragment extends Fragment {
 
-
+    private long workoutID;
     public WorkoutDetailFragment() {
         // Required empty public constructor
     }
@@ -26,4 +27,21 @@ public class WorkoutDetailFragment extends Fragment {
         return inflater.inflate(R.layout.fragment_workout_detail, container, false);
     }
 
+    //получаем ID выбранного комплекса упражнений
+    public void setWorkoutID(long workoutID) {
+        this.workoutID = workoutID;
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        View view = getView();
+        if(view!=null){
+            TextView name = view.findViewById(R.id.workout_title);
+            Workout workoutSelected = Workout.listworkout[(int)workoutID];
+            name.setText(workoutSelected.getName());
+            TextView description = view.findViewById(R.id.workout_description);
+            description.setText(workoutSelected.getDescription());
+        }
+    }
 }

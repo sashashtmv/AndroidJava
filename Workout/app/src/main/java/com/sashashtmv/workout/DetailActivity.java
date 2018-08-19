06fc09cc26369c4b5b6c38059b4c4177e69e75bd
sashashtmv <sasha-shtmv@rambler.ru>
2018-08-19 23:30:45 +0300

    new branch3

--- Workout/app/src/main/java/com/sashashtmv/workout/DetailActivity.java
+++ Workout/app/src/main/java/com/sashashtmv/workout/DetailActivity.java
@@ -5,12 +5,15 @@ import android.os.Bundle;
 
 public class DetailActivity extends AppCompatActivity {
 
+    public static final String EXTRA_WORKOUT_ID = "id";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_detail);
 
         WorkoutDetailFragment detailFragment = (WorkoutDetailFragment) getSupportFragmentManager().findFragmentById(R.id.detail_fragment);
-        detailFragment.setWorkoutID(1);
+        int workoutId = (int) getIntent().getExtras().get(EXTRA_WORKOUT_ID);
+        detailFragment.setWorkoutID(workoutId);
     }
 }
--- Workout/app/src/main/java/com/sashashtmv/workout/WorkoutListFragment.java
+++ Workout/app/src/main/java/com/sashashtmv/workout/WorkoutListFragment.java
@@ -1,6 +1,7 @@
 package com.sashashtmv.workout;
 
 
+import android.content.Context;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.ListFragment;
@@ -8,18 +9,22 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class WorkoutListFragment extends ListFragment {
-
+    //добавляет слушателя к фрагменту
+    static interface Listener {
+        void itemClicked(long id);
+    };
+    private Listener listener;
 
     public WorkoutListFragment() {
         // Required empty public constructor
     }
-
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -36,4 +41,20 @@ public class WorkoutListFragment extends ListFragment {
         return super.onCreateView(inflater, container, savedInstanceState);
     }
 
+    //Вызывается, когда фрагмент связывается с активностью. Напомним, что класс Activity является
+    //субклассом Context
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        this.listener = (Listener)context;
+    }
+
+    //Сообщает слушателю,когда пользователь выбрал вариант в ListView
+    @Override
+    public void onListItemClick(ListView l, View v, int position, long id) {
+        super.onListItemClick(l, v, position, id);
+        if(listener != null){
+            listener.itemClicked(id);
+        }
+    }
 }
